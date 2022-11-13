7b33051715b1092d7f464235ac625f6dd1738c0c
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2022-11-13 22:28:58 +0100

    added githubapi

--- portfolio.js
+++ portfolio.js
@@ -1,17 +1,30 @@
-portfolioSection = document.getElementById("portfolioSection");
+let portfolioSection = document.getElementById("portfolioSection");
 let name = document.getElementById("name");
 
-let url = "#";
-async function getTodoList() {
+let url = "https://api.github.com/users/chasmkhasan/repos";
+async function getportfolioList() {
   let response = await fetch(url);
   if (response.ok) {
     let data = await response.json();
+
     let cardHTML = "";
     data.map((element) => {
-      cardHTML += "";
+      cardHTML +=
+        '<div>\
+        <a class ="download" href= ' +
+        element.html_url +
+        ' target="_blank"> \
+         <h3>' +
+        element.name +
+        "</h3>\
+        <p>"; /* +
+        element.full_name + */
+      ("</p>\
+        </a>\
+        </div>\
+        ");
     });
-    document.getElementsByClassName("portfolio")[0].innerHTML = cardHTML;
+    document.getElementsByClassName("project-file")[0].innerHTML = cardHTML;
   }
 }
-
-getTodoList();
+getportfolioList();
